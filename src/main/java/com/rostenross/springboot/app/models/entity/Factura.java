7deb34f6700ee0d4dc6240dd21e10c5261018c3d
RostenRossDev <costantini.nestor.m@gmail.com>
2020-04-11 22:49:57 +0000

    plantilla-items files was created,   autocoplete js file was modified.

--- src/main/java/com/rostenross/springboot/app/models/entity/Factura.java
+++ src/main/java/com/rostenross/springboot/app/models/entity/Factura.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,11 +15,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.CascadeType;
 
-
+@Entity
+@Table(name="facturas")
 public class Factura implements Serializable {
 
     @Id
