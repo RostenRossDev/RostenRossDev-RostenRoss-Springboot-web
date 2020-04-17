f8ab820022678b376f22407e0713f2d1ce3062ea
RostenRossDev <costantini.nestor.m@gmail.com>
2020-04-17 22:50:59 +0000

    LoginController.java and login.html files was created.

--- src/main/java/com/rostenross/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/rostenross/springboot/app/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -14,17 +15,36 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Autowired
-    public void configuredGlobal(AuthenticationManagerBuilder builder) throws Exception{
-        PasswordEncoder encoder = passwordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode) ;
 
-        builder.inMemoryAuthentication().
-        withUser(users.username("admin").password("1234").roles("ADMIN","USER"))
-        .withUser(users.username("user").password("1234").roles("USER"));
+    @Autowired
+    public void configuredGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        PasswordEncoder encoder = passwordEncoder();
+        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+        builder.inMemoryAuthentication().withUser(users.username("admin").password("1234").roles("ADMIN", "USER"))
+                .withUser(users.username("user").password("1234").roles("USER"));
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/css/**","/js/**","images/**","/listar").permitAll()
+        .antMatchers("/ver/**").hasAnyRole("USER")
+        .antMatchers("/uploads").hasAnyRole("USER")
+        .antMatchers("/form/**").hasAnyRole("ADMIN")
+        .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+        .antMatchers("/factura/**").hasAnyRole("ADMIN")
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+        .and()
+        .logout().permitAll();
+    }
+
+    
     
 }
